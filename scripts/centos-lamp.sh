03de0404b94ddd42bdf4956817dca3ae25c9bc11
Stanley Hughes <stan.hughes@riverbed.com>
2018-03-23 11:23:30 -0700

    Updated LAMP script with GitHub files

--- scripts/centos-lamp.sh
+++ scripts/centos-lamp.sh
@@ -28,5 +28,8 @@ service mysqld start
 mysql -u root -e "SHOW DATABASES";
 
 # Download Starter Content
+cd /vagrant
+sudo -u vagrant wget -q https://raw.githubusercontent.com/sehughes/vagrant/master/files/index.html
+sudo -u vagrant wget -q https://raw.githubusercontent.com/sehughes/vagrant/master/files/info.php
 
 service httpd restart
