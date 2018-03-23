0bcb538ee7a5b9a4cde4a4ee88b6768c75c52421
Stanley Hughes <stan.hughes@riverbed.com>
2018-03-23 12:26:52 -0700

    Adding files for multi-vm setup

---
+++ scripts/centos-web.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Apache
+yum install -y httpd httpd-devel httpd-tools
+chkconfig --add httpd
+chkconfig httpd on
+service httpd stop
+
+# sym link
+rm -rf /var/www/html
+ln -s /vagrant /var/www/html
+
+service httpd start
+
+# PHP
+yum install -y php php-cli php-common php-devel php-mysql
+
+# Download Starter Content
+cd /vagrant
+sudo -u vagrant wget -q https://raw.githubusercontent.com/sehughes/vagrant/master/files/index.html
+sudo -u vagrant wget -q https://raw.githubusercontent.com/sehughes/vagrant/master/files/info.php
+
+service httpd restart
